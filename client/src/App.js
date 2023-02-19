cc9fb4c1deb40c58ebd635b9273e257472f08cb6
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-02-19 20:35:29 +0900

    :lipstick: fix: App.js 라우터 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import "./theme.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuestionAsk from "./pages/QuestionAsk";
 import Questions from "./pages/Questions";
@@ -6,25 +7,35 @@ import UsersPersonalPage from "./pages/UsersPersonalPage";
 import Tags from "./pages/Tags";
 import Users from "./pages/Users";
 import Search from "./components/Search";
-
-import QuestionDetail from './pages/QuestionDetail'
-
+import BaseLayout from "./components/layouts/BaseLayout";
+import MainLayout from "./components/layouts/MainLayout";
+import Guide from "./pages/Guide";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Search />
-                <Routes>
-                    <Route path="/" element={<Questions />} />
-                    <Route path="/ask" element={<QuestionAsk />} />
-                    <Route path="/edit" element={<QuestionEdit />} />
-                    <Route path="/users/1" element={<UsersPersonalPage />} />
-                    <Route path="/tags" element={<Tags />} />
-                    <Route path="/users" element={<Users />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout hasSidebar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/ask" element={<QuestionAsk />} />
+          <Route path="/edit" element={<QuestionEdit />} />
+          <Route path="/guide" element={<Guide />} />
+        </Route>
+        <Route element={<MainLayout />}>
+          <Route path="/tags" element={<Tags />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/1" element={<UsersPersonalPage />} />
+        </Route>
+        <Route element={<BaseLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
