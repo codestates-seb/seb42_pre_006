3ae35c257b08c8dc5ff08705ea85efaca1ebc470
dding-v <v.dding.v@gmail.com>
2023-02-19 10:37:53 +0900

    :lipstick: fix: PostAnswerBox 하단 태그 추가

--- client/src/components/QuestionDetail/Answer/PostAnswerBox.jsx
+++ client/src/components/QuestionDetail/Answer/PostAnswerBox.jsx
@@ -1,14 +1,20 @@
 // Toastinput 감싼 후 YourAnswer
+import { Link } from "react-router-dom";
 
-import Toastinput from './Toastinput'
+import Toastinput from "./Toastinput";
 
 const PostAnswerBox = () => {
-    return (
-        <div className=' flex flex-col items-start mt-16'>
-            <div className=' mb-4 text-lg'>Your Answer</div>
-            <Toastinput />
-        </div>
-    )
-}
+  return (
+    <div className=" flex flex-col items-start mt-16">
+      <div className=" mb-4 text-lg">Your Answer</div>
+      <Toastinput />
+      <div className=" mt-8 text-lg">
+        Browse other questions tagged {/* Tags */} or ask
+        your own question.
+        {/* <Link to="/question">your own question.</Link> */}
+      </div>
+    </div>
+  );
+};
 
-export default PostAnswerBox
+export default PostAnswerBox;
