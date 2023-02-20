3e89f546be83338143ae3dc6768c8c9161a9280c
dding-v <v.dding.v@gmail.com>
2023-02-20 14:37:56 +0900

    :lipstick: style: QuestionAsk 텍스트 정렬 수정

--- client/src/components/QuestionAsk/QuestionAskHeader.jsx
+++ client/src/components/QuestionAsk/QuestionAskHeader.jsx
@@ -1,7 +1,7 @@
 function QuestionAskHeader() {
   return (
     <section>
-      <h1 className="text-3xl font-semibold px-8 py-8">
+      <h1 className="text-3xl font-semibold px-8 py-8 text-left">
         Ask a public question
       </h1>
     </section>
--- client/src/pages/QuestionAsk.jsx
+++ client/src/pages/QuestionAsk.jsx
@@ -3,6 +3,7 @@ import QuestionAskFormBody from '../components/QuestionAsk/QuestionAskFormBody';
 import QuestionAskFormTags from '../components/QuestionAsk/QuestionAskFormTags';
 import QuestionAskFormTitle from '../components/QuestionAsk/QuestionAskFormTitle';
 import QuestionAskHeader from '../components/QuestionAsk/QuestionAskHeader';
+import Button from '../components/UI/Button';
 
 function QuestionAsk() {
   return (
@@ -11,24 +12,16 @@ function QuestionAsk() {
       <QuestionAskFormTitle />
       <QuestionAskFormBody />
       <QuestionAskFormTags />
-      <div className="px-8 absolute right-0 bottom-100">
+      <div className="px-8 absolute right-0 bottom-100 flex">
         {/* TODO: 버튼 라우터기능 작업 -> [Post your question]버튼 클릭시 [질문상세페이지]로 이동, 버튼 타입 [submit]으로 변경 */}
-        <Link to="/" className="mr-3">
-          <button
-            type="button"
-            className="text-sm bg-[#0A95FF] hover:bg-[#0074CC] text-white font-semibold py-2 px-2 border-t-2 border-[#47aefd] hover:border-[#47aefd] rounded"
-          >
-            Post your question
-          </button>
-        </Link>
+        <Button to="/questions" variant="primary" size="md">
+          Post Your Answer
+        </Button>
         <Link to="/">
           {/* TODO: 버튼 기능작업 -> [Cancel]버튼 클릭시 정말 닫을건지 확인하는 모달 창? 프롬프트창? 띄우기 */}
-          <button
-            type="button"
-            className="text-sm hover:bg-[rgba(255,10,10,0.2)] text-[rgb(255,10,10)] font-semibold py-2 px-2 rounded"
-          >
+          <Button variant="danger" size="sm" text>
             Cancel
-          </button>
+          </Button>
         </Link>
         {/* QUEST: Review post question 페이지 구현 X -> 유효성 검사 실시해야하는지? */}
       </div>
