ff6ed62f6df82719dd4dac709e9263f52bb7b8a4
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-02-23 03:46:30 +0900

    :sparkles: feat: 유저 상세 정보 조회 및 수정 기능 추가

---
+++ client/src/pages/UserPersonalEdit.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import UsersPersonalPageHeader from '../components/UsersPersonalPage/UsersPersonalPageHeader';
+import UsersPersonalEditForm from '../components/UsersPersonalPage/UsersPersonalEditForm';
+
+function UserPersonalEdit() {
+  const params = useParams();
+  const [user, setUser] = useState(null);
+
+  const handleGetUserInfo = async () => {
+    try {
+      const response = await axios.get(`/api/v1/members/${params.id}`);
+      if (response) {
+        setUser(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetUserInfo();
+  }, []);
+
+  return (
+    <>
+      {user && (
+        <article className="ml-6">
+          <UsersPersonalPageHeader user={user} />
+          <UsersPersonalEditForm user={user} />
+        </article>
+      )}
+    </>
+  );
+}
+
+export default UserPersonalEdit;
