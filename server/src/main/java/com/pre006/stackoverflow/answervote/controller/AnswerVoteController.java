82a3b35fcfd8d5ad356a9a0657ad3fffbc58f8c9
Kyusung98 <sgs3898@gmail.com>
2023-02-24 11:18:34 +0900

    :sparkles: [BE] feat: AnswerVote 기능구현
    [BE] feat: AnswerVote 기능구현

--- server/src/main/java/com/pre006/stackoverflow/answervote/controller/AnswerVoteController.java
+++ server/src/main/java/com/pre006/stackoverflow/answervote/controller/AnswerVoteController.java
@@ -12,18 +12,22 @@ public class AnswerVoteController {
     private final static String ANSWER_VOTE_DEFAULT_URL = "/answer/{answer-id}/answer-vote";
 
     @PostMapping
-    public ResponseEntity postAnswerVote(@RequestBody AnswerVoteDto.Post post){
+    public ResponseEntity postAnswerVote(@PathVariable("answer-id") Long answerId,
+                                         @RequestBody AnswerVoteDto.Post post){
 
         return new  ResponseEntity<>(HttpStatus.CREATED);
     }
-    @PatchMapping
-    public ResponseEntity patchAnswerVote(@RequestBody AnswerVoteDto.Patch patch){
+    @PatchMapping("/{answer-vote-id}")
+    public ResponseEntity patchAnswerVote(@PathVariable("answer-id") Long answerId,
+                                          @PathVariable("answer-vote-id") Long answerVoteId,
+                                          @RequestBody AnswerVoteDto.Patch patch){
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity deleteAnswerVote(){
+    @DeleteMapping("/{answer-vote-id}")
+    public ResponseEntity deleteAnswerVote(@PathVariable("answer-id") Long answerId,
+                                           @PathVariable("answer-vote-id") Long answerVoteId){
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
