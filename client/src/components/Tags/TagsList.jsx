6c24891248979acc19eb1caa3dd63084da0b14fb
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-02-27 21:53:52 +0900

    :bug: fix: 태그 목록, 태그 설명 글 추가 및 주석 처리

--- client/src/components/Tags/TagsList.jsx
+++ client/src/components/Tags/TagsList.jsx
@@ -10,7 +10,6 @@ function TagsList() {
       const { data } = response;
       if (data) {
         setTgas(data.data);
-        console.log(data.data);
       }
     } catch (error) {
       console.error(error);
@@ -24,21 +23,16 @@ function TagsList() {
   return (
     <section className="mt-4">
       {tags && (
-        <ul className="grid grid-cols-4 items-center text-left gap-3">
+        <ul className="grid grid-cols-4 items-stretch text-left gap-3">
           {tags?.map(tag => (
             <li className="" key={tag.tagId}>
-              <div className=" border text-sm p-4">
+              <div className=" border text-sm p-4 h-full min-h-[10rem]">
                 <span className="rounded bg-[#E1ECF4] py-1 px-2 text-blue-900">
                   {tag.tagName}
                 </span>
-                <p className="mt-5 line-clamp-4">
-                  For questions about programming in ECMAScript (JavaScript/JS)
-                  and its different dialects/implementations (except for
-                  ActionScript). Keep in mind that JavaScript is NOT the same as
-                  Java! Include all labels that are relevant to your question;
-                  e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular],
-                  [ember.js], [vue.js], [typescript], [svelte], etc.
-                </p>
+                {tag.tagInfo && tag.tagInfo.length > 0 && (
+                  <p className="mt-5 line-clamp-4">{tag.tagInfo}</p>
+                )}
               </div>
             </li>
           ))}
